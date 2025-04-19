0692297317af74ec9cad032e2b98b716aad6b2d8
tienphat140205 <159804034+tienphat140205@users.noreply.github.com>
2025-04-19 17:31:41 +0700

    update

--- models/test.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.path.append('/content/drive/MyDrive/projects/DualTopic/')
-from models.DualTopic import DualTopic
--- run.sh
+++
@@ -1,9 +0,0 @@
-# /bin/bash
-set -e
-
-
-python crosslingual_topic_model.py --model DualTopic --dataset ECNews --weight_MI 30.0
-
-python crosslingual_topic_model.py --model DualTopic --dataset AmazonReview --weight_MI 50.0
-
-python crosslingual_topic_model.py --model DualTopic --dataset RakutenAmazon --weight_MI 50.0
